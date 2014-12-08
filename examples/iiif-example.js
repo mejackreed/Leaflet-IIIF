b7603960e58ef6a9dd02cbe9520cff4176da0dcd
Jack Reed <pjreed@stanford.edu>
2014-12-08 08:23:23 -0800

    checks tile sizes and changes styles

--- examples/iiif-example.js
+++ examples/iiif-example.js
@@ -5,4 +5,5 @@ var map = L.map('map', {
 });
 // http://iiif.klokantech.com/demo.jp2
 // http://libimages.princeton.edu/loris2/pudl0076%2Fmap_pownall%2F00000001.jp2
-L.tileLayer.iiif('http://libimages.princeton.edu/loris2/pudl0076%2Fmap_pownall%2F00000001.jp2/info.json').addTo(map);
+// https://stacks.stanford.edu/image/iiif/hg676jb4964%2F0380_796-44/info.json
+L.tileLayer.iiif('https://stacks.stanford.edu/image/iiif/hg676jb4964%2F0380_796-44/info.json').addTo(map);
