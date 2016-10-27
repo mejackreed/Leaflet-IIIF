59f9f5e4bda98df2c2eeef6fdc0899b5a1b761bd
Jack Reed <mejackreed@users.noreply.github.com>
2016-10-27 10:26:43 -0400

    Revert "add twitter card md"

--- examples/index.js
+++ examples/index.js
@@ -1,5 +1,4 @@
 var map;
-var layer;
 
 map = L.map('map', {
   center: [0, 0],
@@ -7,7 +6,7 @@ map = L.map('map', {
   zoom: 0
 });
 
-var url = window.location.search.replace('?url=', '');
+var url = window.location.search.replace('?url=', '')
 
 // http://stackoverflow.com/a/18222306
 var urlregex = /^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/;
@@ -15,11 +14,3 @@ var urlregex = /^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#
 if (urlregex.test(url)) {
   layer = L.tileLayer.iiif(url, {}).addTo(map);
 }
-
-layer.on('load', function() {
-  $('head').append(
-    '<meta name="twitter:card" content="summary_large_image">' +
-    '<meta name="twitter:site" content="@mejackreed">' +
-    '<meta name="twitter:image" content="' + url.replace('info.json', 'full/' + layer._imageSizes[1].x + ',/0/default.jpg') + '">'
-  );
-});
